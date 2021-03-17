905fb71017489c0c9e200558960cca4bf0707e5d
laecio22 <laecon22@hotmail.com>
2021-03-17 15:01:25 -0300

    Requisição na tela de Home  e mostrar os cards com imagem do pokémon e botões de adicionar e detalhar

--- pokemuyembe/src/Pages/HomePage/HomePage.js
+++ pokemuyembe/src/Pages/HomePage/HomePage.js
@@ -1,15 +1,32 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
 import {useHistory} from "react-router-dom";
-import {goToPokedexPage, goToPokemonDetailPage} from "../../Routes/Coordinator";
+import {goToPokedexPage} from "../../Routes/Coordinator";
 import PokemonCard from "../../Components/PokemonCard/PokemonCard";
+import axios from 'axios'
 
 export default function HomePage() {
+    const [pokemons, setPokemons] = useState([])
     const history = useHistory();
+
+    useEffect(()=>{
+        axios.get('https://pokeapi.co/api/v2/pokemon')
+        .then((response)=>{            
+            setPokemons(response.data.results)
+        })
+        .catch((error) =>{
+            console.log(error)
+        })
+    }, [])
+
+    const listPokemons = pokemons.map((pokemon)=>{         
+         return <PokemonCard url={pokemon.url}/>
+    })
+    
     return (
         <div className="home-container">
             <h1>Lista de Pokemons API</h1>
-            <button onClick={() =>goToPokedexPage(history)}>Ir para Pokedex</button>
-            <button onClick={()=>goToPokemonDetailPage(history)}>Detalhar pokemon</button>
+            <button onClick={() =>goToPokedexPage(history)}>Ir para Pokedex</button>            
+            {listPokemons}
         </div>
     )
 }
