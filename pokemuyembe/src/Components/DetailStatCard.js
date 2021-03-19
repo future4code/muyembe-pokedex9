f5b4241a921ff2f3ae07910e9100e5fa9545497d
marivellar <mariana.ralleva@gmail.com>
2021-03-18 23:10:33 -0300

    não consegui implementar os detalhes do pokemon, erro no useParam

---
+++ pokemuyembe/src/Components/DetailStatCard.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import React,{useState,useEffect} from 'react';
+
+
+const DetailStatCard = (props) => {
+    const [nameStat, setNameStat] = useState("")
+    const [valueStat, setValueStat] = useState("")
+    
+    useEffect(() => {
+        axios.get(`${props.stat}`)
+        .then((response) => {
+            setNameStat(response.data.name)
+            setValueStat(response.data.game_index)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    }), [props.stat]
+
+    return ( <div>
+        <p>{nameStat}</p>
+        <p>{valueStat}</p>
+    </div>
+
+    )
+}
+
+export default DetailStatCard;
